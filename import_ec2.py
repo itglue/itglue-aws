210b5fb17f709560f8a3442af5706c97ad4cce68
Lisa Hsu <lhsu@itglue.com>
2018-07-10 11:55:44 -0700

    refactor to allow update on single instance and add flag to allow update on all instances

--- import_ec2.py
+++ import_ec2.py
@@ -5,6 +5,7 @@ import translator
 import itglue
 from multiprocessing import Process
 import argparse
+import sys
 
 class EC2ImportError(Exception):
     pass
@@ -20,50 +21,61 @@ def get_organization(org_id_or_name):
             raise EC2ImportError('Organization with name {} not found'.format(org_name))
         return orgs[0]
 
-def import_ec2_instances(organization, ignore_locations=False):
-    instances = get_instances()
+def import_ec2_instances(organization, ignore_locations=False, instance_id=None):
     active_status = itglue.ConfigurationStatus.first_or_create(name='Active')
     inactive_status = itglue.ConfigurationStatus.first_or_create(name='Inactive')
     ec2_type = itglue.ConfigurationType.first_or_create(name='EC2')
 
-    # create a dict to memoize the locations
-    locations_dict = {}
-
     # create a list to keep all processes
     processes = []
 
-    for instance in instances:
-        kwargs = {
-            'organization': organization,
-            'instance': instance,
-            'conf_type': ec2_type,
-            'active_status': active_status,
-            'inactive_status': inactive_status
-        }
-        if not ignore_locations:
-            location_translator = translator.PlacementTranslator(instance.placement)
-            location_name = location_translator.translate('name')
-            if locations_dict.get(location_name):
-                kwargs['location'] = locations_dict[location_name]
-            else:
-                location_attributes = location_translator.translated
-                location = itglue.Location.first_or_create(organization_id=organization.id, **location_attributes)
-                locations_dict[location_name] = location
-                kwargs['location'] = location
-        process = Process(target=update_configuration_and_interfaces, kwargs=kwargs)
-        processes.append(process)
+    kwargs = {
+        'organization': organization,
+        'conf_type': ec2_type,
+        'active_status': active_status,
+        'inactive_status': inactive_status
+    }
 
+    if instance_id:
+        instance = get_instances(instance_id)
+        process = configure_instance(kwargs, instance, ignore_locations, organization.id)
+        processes.append(process)
+    else:
+        instances = get_instances()
+        for instance in instances:
+            process = configure_instance(kwargs, instance, ignore_locations, organization.id)
+            processes.append(process)
     # start all processes
     for process in processes:
         process.start()
-
     # make sure that all processes have finished
     for process in processes:
         process.join()
 
-def get_instances():
+
+def get_instances(instance_id=None):
     ec2 = boto3.resource('ec2')
+    if instance_id:
+        instance = ec2.Instance(instance_id)
+        return instance
     return ec2.instances.all()
+
+
+def configure_instance(kwargs, instance, ignore_locations, organization_id):
+    locations_dict = {}
+    kwargs['instance'] = instance
+    if not ignore_locations:
+        location_translator = translator.PlacementTranslator(instance.placement)
+        location_name = location_translator.translate('name')
+        if locations_dict.get(location_name):
+            kwargs['location'] = locations_dict[location_name]
+        else:
+            location_attributes = location_translator.translated
+            location = itglue.Location.first_or_create(organization_id=organization_id, **location_attributes)
+            locations_dict[location_name] = location
+            kwargs['location'] = location
+    process = Process(target=update_configuration_and_interfaces, kwargs=kwargs)
+    return process
 
 def update_configuration_and_interfaces(organization, instance, conf_type, active_status, inactive_status, location=None):
     configuration = update_or_create_configuration(
@@ -77,6 +89,7 @@ def update_configuration_and_interfaces(organization, instance, conf_type, activ
     for interface in instance.network_interfaces:
         primary = instance.private_ip_address == interface.private_ip_address
         update_or_create_config_interface(interface, configuration, primary=primary)
+
 
 def update_or_create_configuration(instance, location, organization, conf_type, active_status, inactive_status):
     instance_attributes = translator.EC2Translator(
@@ -108,13 +121,16 @@ def update_or_create_config_interface(interface, configuration, primary=False):
 
 # Command-line functions
 def main():
-    args = parse_args()
+    args = get_args()
     ignore_locations = args.ignore_locations
+    id = args.instance_id
+    if args.add_all and id:
+        id = None
     organization = get_organization(args.organization)
-    import_ec2_instances(organization, ignore_locations=ignore_locations)
+    import_ec2_instances(organization, ignore_locations=ignore_locations, instance_id=id)
     return True
 
-def parse_args():
+def get_args():
     parser = argparse.ArgumentParser(
         description='Import EC2 instances as Configurations into an IT Glue Organization')
     parser.add_argument(
@@ -128,8 +144,20 @@ def parse_args():
         action='store_true',
         help='Do not import EC2 placements as IT Glue Locations'
     )
-    return parser.parse_args()
+    parser.add_argument(
+        '-id', '--instance-id',
+        type=str,
+        help='ID of the instance to be created or updated'
+    )
+    parser.add_argument(
+        '--add-all',
+        action='store_true',
+        help='add all the ec2 instances, will override instance_id'
+    )
+    args = parser.parse_args()
+    if not args.add_all and not args.instance_id:
+        parser.error('Must provide an instance ID or turn on --add-all flag')
+    return args
 
 if __name__ == "__main__":
     main()
-
--- lambda_handler.py
+++ lambda_handler.py
@@ -4,7 +4,8 @@ import os
 def handler(event, context):
     org_name_or_id = os.environ.get('ORGANIZATION')
     organization = get_organization(org_name_or_id)
-    return import_ec2_instances(organization)
+    instance_id = event['detail']['instance-id']
+    return import_ec2_instances(organization, instance_id=instance_id)
 
 if __name__ == "__main__":
     handler(event={}, context=None)
