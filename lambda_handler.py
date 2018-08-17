64c6ee42ade9b78ccc5ac96d57a0ac3bd5ee44c2
Lisa Hsu <lhsu@itglue.com>
2018-08-17 11:44:49 -0700

    ITG-4782 update handler and zip to include new translator folder

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,11 +1,15 @@
-from import_ec2 import get_organization, import_ec2_instances
+from import_ec2 import import_ec2_instances
+from importer_wrapper import get_organization
 import os
 
+
 def handler(event, context):
+
     org_name_or_id = os.environ.get('ORGANIZATION')
     organization = get_organization(org_name_or_id)
     instance_id = event['detail']['instance-id']
     return import_ec2_instances(organization, instance_id=instance_id)
+
 
 if __name__ == "__main__":
     handler(event={}, context=None)
