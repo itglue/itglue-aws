e29e6a56199517e52ba0f128478d4f4901f60c77
Lisa Hsu <lhsu@itglue.com>
2018-08-30 17:44:55 -0700

    ITG-4876 split lambda config file into separate template pieces and add CLI ability to spin up cloudformation stacks based on user input

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,15 +1,20 @@
 from import_ec2 import import_ec2_instances
+from import_workspace import import_workspaces
 from itglue_adapter import get_organization
 import os
 
 
-def handler(event, context):
-
-    org_name_or_id = os.environ.get('ORGANIZATION')
-    organization = get_organization(org_name_or_id)
+def ec2_handler(event, context):
+    organization = get_org()
     instance_id = event['detail']['instance-id']
     return import_ec2_instances(organization, instance_id=instance_id)
 
 
-if __name__ == "__main__":
-    handler(event={}, context=None)
+def workspace_handler(event, context):
+    organization = get_org()
+    return import_workspaces(organization)
+
+
+def get_org():
+    org_name_or_id = os.environ.get('ORGANIZATION')
+    return get_organization(org_name_or_id)
